89cc262bc02ea54bef6bb9202aaea2cc9d705245
Nayla2102 <nayla_larasati_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:48:07 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,9 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Halo");
+        System.out.println("Nama saya Nayla Larasati");
+        System.out.println("Alamat rumah saya di Jl.Lowokdoro perum City Side blok J2 no 2");
+        System.out.println("Saya dulu bersekolah di SMP Negeri 10 Malang");
+        System.out.println("Nomor hp saya: 081321719251");
+    }
+}
